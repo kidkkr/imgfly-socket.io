3d4ffab1acadffd5950dc97091ef43cb0e17636b
Luke Kang <luk3kang@gmail.com>
2021-03-07 18:27:59 +0900

    Fix new lines

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import { Server } from 'socket.io'
 import * as dotenvFlow from 'dotenv-flow'
 
 dotenvFlow.config()
-
 const PORT = process.env.PORT
 const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN
+
 const httpServer = createServer()
+
 const io = new Server(httpServer, {
   cors: {
     origin: ALLOWED_ORIGIN,
   },
 })
-
 // @TODO: shared type-def of e
 const broadcastDraw = (e: any) => io.sockets.emit('draws', e)
 io.on('connection', (s) => {
